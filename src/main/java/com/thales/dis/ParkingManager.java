382695335f417b4ea8a8db0ba858cce4d5137353
José Pascual Gimeno Marí <tirantvcia@gmail.com>
2023-05-20 17:41:57 +0200

    green,in ParkingManagerTest ParkingOneMotoInParkingManager method

--- src/main/java/com/thales/dis/ParkingManager.java
+++ src/main/java/com/thales/dis/ParkingManager.java
@@ -1,18 +1,51 @@
 package com.thales.dis;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.Optional;
 
 public class ParkingManager {
+
+    ArrayList<SpotFloorManager> spotFloors = null;
+
     public void generate(int numberOfFloorSteps, int numberOfSpotLinesPerFloor, Map<SpotType, Integer> numberSpotsByType) {
+        reset();
+        for(int i = 0; i < numberOfFloorSteps; i++) {
+            addSpotFloor(numberOfSpotLinesPerFloor, numberSpotsByType);
+        }
+    }
+
+    private void reset() {
+        spotFloors = new ArrayList<>();
+    }
+
+    private void addSpotFloor(int numberOfSpotLinesPerFloor, Map<SpotType, Integer> numberSpotsByType) {
+        SpotFloorManager spotFloorManager = new SpotFloorManager();
+        spotFloorManager.generate(numberOfSpotLinesPerFloor, numberSpotsByType);
+        spotFloors.add(spotFloorManager);
     }
 
     public Map<VehiclesType, Integer> getPossibleVehiclesCapacityByType() {
-        Map<VehiclesType, Integer> mapVehicles = new HashMap<>();
-        mapVehicles.put(VehiclesType.CAR, 2);
-        mapVehicles.put(VehiclesType.MOTORCYCLE, 3);
-        mapVehicles.put(VehiclesType.BUS, 0);
-        return mapVehicles;
+        Optional<VehiclesCapacityInFreeSpots> reduce = spotFloors.stream().map(sp -> sp.getPossibleVehiclesCapacityByType()).reduce((a, b) -> a.append(b));
+        if(reduce.isPresent()) {
+            return reduce.get().getVehicles();
+        }
+        return null;
+    }
+
+    public boolean parkVehicle(VehiclesType vehiclesType) {
+        Optional<SpotFloorManager> vehicleIsCorrectlyParked = getTheFirstOneSpotLinesWhereVehicleIsParked(vehiclesType);
+        return vehicleIsCorrectlyParked.isPresent();
+    }
+
+    private Optional<SpotFloorManager> getTheFirstOneSpotLinesWhereVehicleIsParked(VehiclesType vehiclesType) {
+        SpotFloorManager managerWhereVehicleIsParked = null;
+        for(SpotFloorManager manager:spotFloors) {
+            if(manager.parkVehicleInFloorSpot(vehiclesType)) {
+                managerWhereVehicleIsParked = manager;
+                break;
+            }
+        }
+        return Optional.ofNullable(managerWhereVehicleIsParked);
     }
 }
--- src/test/java/com/thales/dis/ParkingManagerTest.java
+++ src/test/java/com/thales/dis/ParkingManagerTest.java
@@ -22,5 +22,46 @@ public class ParkingManagerTest {
         assertEquals(2, manager.getPossibleVehiclesCapacityByType().get(VehiclesType.CAR));
         assertEquals(0, manager.getPossibleVehiclesCapacityByType().get(VehiclesType.BUS));
     }
+    @Test
+    void generationOfParkingWithTwoFloorIndicatedInMap() {
+        ParkingManager manager = new ParkingManager();
+        int numberOfSpotLinesPerFloor = 1;
+        int numberOfFloorSteps = 2;
+        Map<SpotType, Integer> numberSpotsByType = new HashMap<>();
+        numberSpotsByType.put(SpotType.MOTORCYCLE_TYPE, 1);
+        numberSpotsByType.put(SpotType.COMPACT_TYPE, 1);
+        numberSpotsByType.put(SpotType.LARGE_TYPE, 1);
+        manager.generate(numberOfFloorSteps, numberOfSpotLinesPerFloor, numberSpotsByType);
+        assertEquals(6, manager.getPossibleVehiclesCapacityByType().get(VehiclesType.MOTORCYCLE));
+        assertEquals(4, manager.getPossibleVehiclesCapacityByType().get(VehiclesType.CAR));
+        assertEquals(0, manager.getPossibleVehiclesCapacityByType().get(VehiclesType.BUS));
+    }
+    @Test
+    void generationOfParkingWithTwoFloorTwoLinesIndicatedInMap() {
+        ParkingManager manager = new ParkingManager();
+        int numberOfSpotLinesPerFloor = 2;
+        int numberOfFloorSteps = 2;
+        Map<SpotType, Integer> numberSpotsByType = new HashMap<>();
+        numberSpotsByType.put(SpotType.MOTORCYCLE_TYPE, 1);
+        numberSpotsByType.put(SpotType.COMPACT_TYPE, 1);
+        numberSpotsByType.put(SpotType.LARGE_TYPE, 1);
+        manager.generate(numberOfFloorSteps, numberOfSpotLinesPerFloor, numberSpotsByType);
+        assertEquals(12, manager.getPossibleVehiclesCapacityByType().get(VehiclesType.MOTORCYCLE));
+        assertEquals(8, manager.getPossibleVehiclesCapacityByType().get(VehiclesType.CAR));
+        assertEquals(0, manager.getPossibleVehiclesCapacityByType().get(VehiclesType.BUS));
+    }
 
+    @Test
+    void ParkingOneMotoInParkingManager() {
+        ParkingManager manager = new ParkingManager();
+        int numberOfSpotLinesPerFloor = 2;
+        int numberOfFloorSteps = 2;
+        Map<SpotType, Integer> numberSpotsByType = new HashMap<>();
+        numberSpotsByType.put(SpotType.MOTORCYCLE_TYPE, 1);
+        numberSpotsByType.put(SpotType.COMPACT_TYPE, 1);
+        numberSpotsByType.put(SpotType.LARGE_TYPE, 1);
+        manager.generate(numberOfFloorSteps, numberOfSpotLinesPerFloor, numberSpotsByType);
+        assertEquals(true, manager.parkVehicle(VehiclesType.MOTORCYCLE));
+        assertEquals(11, manager.getPossibleVehiclesCapacityByType().get(VehiclesType.MOTORCYCLE));
+    }
 }
